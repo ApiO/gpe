a169fb95d1c5e3f0deab0d95b175795070e5647b
Drio <Drio@localhost>
2012-09-11 17:41:01 +0200

    ajout build win32 + platform msvc2010

---
+++ demo/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//#include "ressource_manager.h"
+
+int main( int argc, const char* argv[] )
+{
+	int age;
+	printf( "\nHello World\n\n" );
+	scanf( "%i", &age );
+}
---
+++ gpe/src/ressource_manager.c
@@ -0,0 +1,14 @@
+#include "ressource_manager.h"
+
+int calculate()
+{
+	int i;
+	int j = 0;
+	for( i = 0; i < 10; i++ ) 
+	{
+		j += i;
+	}
+	return j;
+
+
+}
