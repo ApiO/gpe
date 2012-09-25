569bdd3c8a935357c8ad69309acf164fe817ae95
Drio <Drio@localhost>
2012-09-25 13:28:09 +0200

    array max len ? 30.001 fix?

--- test/vecCompare/main.c
+++ test/vecCompare/main.c
@@ -6,37 +6,41 @@
 
 int main( int argc, const char* argv[] )
 {	
-	const int ITEM_COUNT = 50000; 
-	int i, j;
-	clock_t start, end;
+    const int ITEM_COUNT = 30001; 
+    int i, j;
+    clock_t start, end;
 	
-	cpVect a[ITEM_COUNT], b[ITEM_COUNT];
+    cpVect a[ITEM_COUNT], b[ITEM_COUNT];
 
-	//init
-	for(i = 0; i < ITEM_COUNT; i++)
-	{
-		a[i] = cpVect();
-		a[i].x = rand();
-		a[i].y = rand();
+    //init
+    for(i = 0; i < ITEM_COUNT; i++)
+    {
+        a[i] = cpVect();
+        a[i].x = rand();
+        a[i].y = rand();
 
-		b[i] = cpVect();
-		b[i].x = rand();
-		b[i].y = rand();
-	}
+        b[i] = cpVect();
+        b[i].x = rand();
+        b[i].y = rand();
+    }
 
-	start = clock() / (CLOCKS_PER_SEC / 1000);
+    start = clock();
 
 	
-	//calc
-	for(i = 0; i < ITEM_COUNT; i++)
-	{
-		cpVect result = cpvadd(a[i], b[i]);
-	}
+    //calc
+    for(j = 0; j < ITEM_COUNT; j++)
+    {
+        for(i = 0; i < ITEM_COUNT; i++)
+        {
+            cpVect result = cpvadd(a[i], b[i]);
+        }
+    }
 	
-	end = clock() / (CLOCKS_PER_SEC / 1000);
-	printf("done in: %f ms.\n", end-start);
+    end = clock();
 
-	scanf_s("%d\n", &i);
+    printf("done in: %d ms.\n", (end-start)/ (CLOCKS_PER_SEC / 1000));
 
-	return 0;
+    scanf_s("%d\n", &i);
+
+    return 0;
 }
