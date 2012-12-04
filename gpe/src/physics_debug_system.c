c1dad56937397f55831881925fa66cb763395125
Drio <Drio@localhost>
2012-12-04 10:29:42 +0100

    update GLFW : passage de 2.7.6 -> 2.7.7   maj de GLFW.lib pour win32 : le header ne change pas

--- gpe/src/physics_debug_system.c
+++ gpe/src/physics_debug_system.c
@@ -33,11 +33,11 @@ void physics_debug_system_draw (physics_debug_system * system)
 
 void reshape(int width, int height)
 {
-	glViewport(0, 0, width, height);
-	
 	GLfloat scale = (GLfloat)cpfmin(width/640.0, height/480.0);
 	double hw = width*(0.5/scale);
 	double hh = height*(0.5/scale);
+
+	glViewport(0, 0, width, height);
 	
 	ChipmunkDebugDraw_SetPointLineScale(scale);
 	glLineWidth(scale);
